5167a3d4a30a0b31d0747e9903cce136f6705385
Hugo Salino <144286137+Huguinhu@users.noreply.github.com>
2024-03-12 18:50:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let slideIndex = 0;
+let slideIndex = 1;
 
 function showSlides() {
     let slides = document.getElementsByClassName("mySlides");
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Troca de imagem a cada 2 segundos
 }
 
-showSlides(); // Inicia o slideshow automaticamente
+function plusSlides(n) {
+    slideIndex += n;
+    showSlides();
+}
 
-
+showSlides(); // Mostra a primeira imagem ao carregar a página
