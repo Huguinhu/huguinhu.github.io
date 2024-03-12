833000420548e3a2e9c2595fa9e4786236e61fb5
Hugo Salino <144286137+Huguinhu@users.noreply.github.com>
2024-03-12 18:39:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-function showContent(id) {
-  /* Insira as alterações no seu arquivo JavaScript existente */
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('nav ul li a').forEach(function(link) {
-        link.addEventListener('mouseover', function() {
-            this.style.backgroundColor = '#fff'; /* Fundo branco ao passar o mouse */
-            this.style.color = '#000'; /* Texto preto ao passar o mouse */
-        });
-        link.addEventListener('mouseout', function() {
-            this.style.backgroundColor = 'transparent'; /* Remove o fundo ao tirar o mouse */
-            this.style.color = '#000'; /* Texto preto ao tirar o mouse */
-        });
-    });
-});
 let slideIndex = 0;
 
 function showSlides() {
